64da88f00ddee57dfe9a5675eff7ae3e24fb7797
DishantBatraDev <dishant.batra2015@gmail.com>
2020-09-21 17:57:21 +0530

    Simple Notification Service send notification to multiple receivers.

--- src/lambda/s3/sendNotifications.ts
+++ src/lambda/s3/sendNotifications.ts
@@ -1,4 +1,4 @@
-import {S3Handler, S3Event} from 'aws-lambda'
+import {SNSEvent,SNSHandler, S3Event} from 'aws-lambda'
 import 'source-map-support/register'
 import * as AWS from 'aws-sdk'
 
@@ -17,18 +17,30 @@ const connectionParams = {
 
 const apiGateway =  new AWS.ApiGatewayManagementApi(connectionParams) // for send data to the client
 
-export const handler: S3Handler = async(event: S3Event)=>{
-    for(const record of event.Records){
+export const handler: SNSHandler = async(event: SNSEvent)=>{
+    console.log('Processing SNS event ', JSON.stringify(event))
+
+    for(const snsRecord of event.Records){
+        const s3EventStr = snsRecord.Sns.Message
+        console.log('Processing S3 event',s3EventStr)
+        const s3Event = JSON.parse(s3EventStr)
+
+        await processS3Event(s3Event)
+
+    }
+}
+
+async function processS3Event(s3Event: S3Event){
+    for(const record of s3Event.Records){
         const key = record.s3.object.key
-        console.log('Processing S# item with key: ', key)
-
+        console.log('processing S3 item with key: ', key )
         const connections = await docClient.scan({
-            TableName:connectionsTable
+            TableName: connectionsTable
         }).promise()
-
-        const payload={
-            imageId:key
-        }
+        const payload ={
+            imageId: key
+        }      
+        
         for(const connection of connections.Items){
             const connectionId = connection.id
             await sendMessageToClient(connectionId,payload)
